e8e25164ba4a6f5c4d20bc4a9a6573ea95166185
takumi616 <ohohyeah321@icloud.com>
2024-01-26 02:55:57 +0900

    Add comment

--- service/add_vocabulary.go
+++ service/add_vocabulary.go
@@ -10,5 +10,6 @@ type AddVocabulary struct {
 }
 
 func (a *AddVocabulary) AddVocabulary(ctx context.Context, vocabulary *entity.Vocabulary) (entity.Vocabulary, error) {
+	//Call a store layer's method via interface
 	return a.Store.InsertVocabulary(ctx, vocabulary)
 }
